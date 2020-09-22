cf361931b1bb0234a0498f1282ef43da17d0c96d
smit9025 <smit9025@morris.umn.edu>
2020-09-22 11:38:20 -0500

    Create initial version of array_merge. So far array_merge is supposedly able to merge given arrays into a single array, and then removes duplicates from the resulting array, then returns that array.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,36 @@
 #include "array_merge.h"
-
+#include <stdlib.h>
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+  
+  if ((num_arrays = 0))
+	  return 0;
+
+  int sumSizes = 0;
+  int i = 0;
+  for (i = 0; i < num_arrays; i++)
+	  sumSizes += sizes[i];
+
+  int* arr = (int*) calloc(sumSizes + 1, sizeof(int));
+
+  int j = 0;
+  int l = 0;
+  int m = 0;
+  for (j = 0; j < num_arrays; j++)
+	  for (l = 0; l < sizes[j]; l++){
+		  arr[m] = values[j][l];
+  		  m++;
+	  }
+  mergesort(sumSizes, arr);
+  int n = 0;
+  int o = 0;
+  for (n = 0; n < sumSizes; n++){
+	  for (o = n+1; o < sumSizes; o++){
+		  if(arr[n] == arr[o]){
+			  arr[o] = arr[o + 1];
+			  sumSizes--;
+			  o--;
+		  }
+	  }
+  }
+  return arr;
 }
