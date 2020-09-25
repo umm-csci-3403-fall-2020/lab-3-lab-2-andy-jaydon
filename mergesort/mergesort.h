ebdcfbe12d8dc6d52820cca89b91945e9f043f4d
Ellis <wegle030@morris.umn.edu>
2020-09-25 16:28:24 -0500

    Changes to mergesort.h

--- mergesort/mergesort.h
+++ mergesort/mergesort.h
@@ -2,9 +2,15 @@
 #define MERGESORT_H_GUARD
 
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#define LENGTH 100
 #define UNIT_TESTING
 
-void mergesort(int size, int values[]);
+int * divide(int *array, int startIndex, int endIndex);
+void mergeBlocks(int *final, int *start, int end*, int startLen, int endLen);
+void printArray(int array*, int len);
+void mergesort(int *array, int len);
 
 #endif
