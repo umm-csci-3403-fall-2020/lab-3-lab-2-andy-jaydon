bc27453c7ffa70cd89894e441eb96d3f8e5aaab7
smit9025 <smit9025@morris.umn.edu>
2020-09-25 21:53:18 -0500

    Finish array_merge method I finished array_merge method through much trial and error. I also fixed a few easy to see leaks in array_merge_test.cpp

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,36 +1,52 @@
 #include "array_merge.h"
+#include "../mergesort/mergesort.c"
+#include "../mergesort/mergesort.h"
 #include <stdlib.h>
+#include <stdio.h>
+
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  
-  if ((num_arrays = 0))
-	  return 0;
-
+//Check for no arrays and return zero
+//  if ((num_arrays = 0))
+//	  return 0;
+//Initialize variable for finding sum of sizes
   int sumSizes = 0;
-  int i = 0;
-  for (i = 0; i < num_arrays; i++)
-	  sumSizes += sizes[i];
 
-  int* arr = (int*) calloc(sumSizes + 1, sizeof(int));
-
-  int j = 0;
-  int l = 0;
+  for (int i = 0; i < num_arrays; i++)
+	  sumSizes = sumSizes + sizes[i];
+//Allocate memory to array to hold values
+  int* AnArr = (int*) calloc(sumSizes, sizeof(int));
+//Store values in values inside new array
   int m = 0;
-  for (j = 0; j < num_arrays; j++)
-	  for (l = 0; l < sizes[j]; l++){
-		  arr[m] = values[j][l];
+  for (int j = 0; j < num_arrays; j++)
+	  for (int l = 0; l < sizes[j]; l++){
+		  AnArr[m] = values[j][l];
   		  m++;
 	  }
-  mergesort(sumSizes, arr);
-  int n = 0;
-  int o = 0;
-  for (n = 0; n < sumSizes; n++){
-	  for (o = n+1; o < sumSizes; o++){
-		  if(arr[n] == arr[o]){
-			  arr[o] = arr[o + 1];
-			  sumSizes--;
-			  o--;
+//Run mergesort on created array
+  mergesort(sumSizes, AnArr);
+//Total duplicats in array
+  int dupNums = 0;
+  for (int n = 0; n < sumSizes - 1; n++){
+		  if(AnArr[n] == AnArr[n + 1]){
+			  dupNums++;
 		  }
-	  }
   }
-  return arr;
+
+  int uniqueNums = sumSizes - dupNums;
+//Create final array
+  int* resultArr = (int*) calloc(uniqueNums+1, sizeof(int));
+//Set first array value to the amount of unique characters
+  resultArr[0] = uniqueNums;
+//Needed line because single value test isn't stored properly 
+  resultArr[1] = AnArr[0];
+//Store unique values in new array
+  int p = 2;
+  for(int o = 1; o < sumSizes; o++) {
+	if(AnArr[o] != AnArr[o-1]){
+		resultArr[p] = AnArr[o];
+		p++;
+	}
+  }
+  free(AnArr);
+  return resultArr;
 }
